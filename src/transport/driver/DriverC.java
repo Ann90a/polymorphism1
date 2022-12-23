0578cb7f5e2261ffc0d0b5e7dc28fac82a417c38
Ann90a <abrosimova222@yandex.ru>
2022-12-24 01:16:06 +0300

    Полиморфизм выполнено

---
+++ src/transport/driver/DriverC.java
@@ -0,0 +1,26 @@
+package transport.driver;
+
+import transport.Car;
+import transport.Truck;
+
+public class DriverC extends Driver<Truck>{
+    public DriverC(String name, boolean hasDriverLicense, int experience) {
+        super(name, hasDriverLicense, experience);
+    }
+
+    @Override
+    public void startMove(Truck transport) {
+        transport.start();
+    }
+
+    @Override
+    public void stopMove(Truck transport) {
+        transport.stop();
+    }
+
+    @Override
+    public void refill(Truck transport) {
+        System.out.println("Водитель " + getName() + " заправляет грузовик " + transport.getBrand());
+    }
+}
+
